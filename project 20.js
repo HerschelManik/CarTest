83ce3bd736f4017248b4b2a546666cd6e1058fea
HerschelManik <70515255+HerschelManik@users.noreply.github.com>
2020-09-18 21:50:02 +0530

    Add files via upload

---
+++ project 20.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+var car,wall;
+var speed,weight;
+
+
+speed = random(55,98)
+weight = random(400,1500)
+
+
+car = (380,200,20,20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+
+
+display.car()
+
